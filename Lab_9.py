b56f17a2742f7124e1d825464caeec4c7604f833
rritam94 <rritam94@gmail.com>
2024-07-23 18:21:34 -0400

    added main function

--- Lab_9.py
+++ Lab_9.py
@@ -5,3 +5,31 @@ def encode(password):
         new_password += (str((int(char)+ 3) % 10))
 
     return new_password
+
+def main():
+    while(True):
+        encoded_value = 0
+        decoded_value = 0
+
+        print('Menu')
+        print('-------------')
+        print('1. Encode')
+        print('2. Decode')
+        print('3. Quit\n')
+
+        option = int(input('Please enter an option: '))
+        
+        if option == 1:
+            pw = input('Please enter the password to encode: ')
+            encoded_value = encode(pw)
+            print('Your password has been encoded and stored!')
+        
+        elif option == 2:
+            decoded_value = decode(encoded_value)
+            print('The encoded password is', str(encoded_value) + ', and the original password is', str(decoded_value) + '.')
+
+        elif option == 3:
+            break
+
+if __name__ == '__main__':
+    main()
