a716275726afa39eb8d1f39ee44a2d0bb0822bc4
Alex Atia <alexatia@Alexs-MacBook-Pro-8.local>
2024-07-23 14:16:31 -0400

    added decode function

--- Lab_9.py
+++ Lab_9.py
@@ -5,3 +5,10 @@ def encode(password):
         new_password += (str((int(char)+ 3) % 10))
 
     return new_password
+
+def decode(password):
+    decoded_password = ''
+    for char in password:
+        decoded_password += str(((int(char)+10)-3) % 10)
+
+    return decoded_password
